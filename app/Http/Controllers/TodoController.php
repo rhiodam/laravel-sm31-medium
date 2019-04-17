a3e650611e001a2ec77d59e75613e37e9f4c344f
Rhioda muthie <rhioda.m@gmail.com>
2019-04-17 18:55:25 +0700

    add crud todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -15,6 +15,10 @@ class TodoController extends Controller
     public function index()
     {
         //
+        $todos = Todo::orderBy('created_at','desc')->paginate(8);
+        return view('todos.index',[
+            'todos' => $todos,
+        ]);
     }
 
     /**
@@ -25,6 +29,7 @@ class TodoController extends Controller
     public function create()
     {
         //
+        return view('todos.create');
     }
 
     /**
@@ -36,6 +41,32 @@ class TodoController extends Controller
     public function store(Request $request)
     {
         //
+        //validation rules
+        $rules = [
+            'title' => 'required|string|unique:todos,title|min:2|max:191',
+            'body'  => 'required|string|min:5|max:1000',
+        ];
+        //custom validation error messages
+        $messages = [
+            'title.unique' => 'Todo title should be unique', //syntax: field_name.rule
+        ];
+        //First Validate the form data
+        $request->validate($rules,$messages);
+        //Create a Todo
+        $todo        = new Todo;
+        $todo->title = $request->title;
+        $todo->body  = $request->body;
+        $todo->user_id = Auth::id();
+        $todo->save(); // save it to the database.
+        //Redirect to a specified route with flash message.
+        return redirect()
+            ->route('todos.index')
+            ->with('status','Created a new Todo!');
+
+//        return redirect('/todos'); // to a specific url
+//        return redirect(url('/todos')); // to a specific url with url helper
+//        return redirect(url()->previous()); // to a previous url
+//        return redirect()->back(); // redirect back (same as above)
     }
 
     /**
@@ -44,9 +75,13 @@ class TodoController extends Controller
      * @param  \App\Todo  $todo
      * @return \Illuminate\Http\Response
      */
-    public function show(Todo $todo)
+    public function show($id)
     {
         //
+        $todo = Todo::findOrFail($id);
+        return view('todos.show',[
+            'todo' => $todo,
+        ]);
     }
 
     /**
@@ -55,9 +90,14 @@ class TodoController extends Controller
      * @param  \App\Todo  $todo
      * @return \Illuminate\Http\Response
      */
-    public function edit(Todo $todo)
+    public function edit($id)
     {
         //
+        //Find a Todo by it's ID
+        $todo = Todo::findOrFail($id);
+        return view('todos.edit',[
+            'todo' => $todo,
+        ]);
     }
 
     /**
@@ -67,9 +107,29 @@ class TodoController extends Controller
      * @param  \App\Todo  $todo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Todo $todo)
+    public function update(Request $request, $id)
     {
         //
+        //validation rules
+        $rules = [
+            'title' => "required|string|unique:todos,title,{$id}|min:2|max:191", //Using double quotes
+            'body'  => 'required|string|min:5|max:1000',
+        ];
+        //custom validation error messages
+        $messages = [
+            'title.unique' => 'Todo title should be unique',
+        ];
+        //First Validate the form data
+        $request->validate($rules,$messages);
+        //Update the Todo
+        $todo        = Todo::findOrFail($id);
+        $todo->title = $request->title;
+        $todo->body  = $request->body;
+        $todo->save(); //Can be used for both creating and updating
+        //Redirect to a specified route with flash message.
+        return redirect()
+            ->route('todos.show',$id)
+            ->with('status','Updated the selected Todo!');
     }
 
     /**
@@ -78,8 +138,15 @@ class TodoController extends Controller
      * @param  \App\Todo  $todo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Todo $todo)
+    public function destroy($id)
     {
         //
+        //Delete the Todo
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+        //Redirect to a specified route with flash message.
+        return redirect()
+            ->route('todos.index')
+            ->with('status','Deleted the selected Todo!');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}"> {{-- <- bootstrap css --}}
     <title>@yield('title','Laravel 5.8 Basics')</title>
+    <style>
+        .alert{
+            z-index: 99;
+            top: 60px;
+            right:18px;
+            min-width:30%;
+            position: fixed;
+            animation: slide 0.5s forwards;
+        }
+        @keyframes slide {
+            100% { top: 30px; }
+        }
+        @media screen and (max-width: 668px) {
+            .alert{ /* center the alert on small screens */
+                left: 10px;
+                right: 10px;
+            }
+        }
+    </style>
 </head>
 <body>
 {{-- That's how you write a comment in blade --}}
@@ -16,7 +35,27 @@
     @yield('content')
 </main>
 @include('inc.footer')
+
+@if(session('status')) {{-- <- If session key exists --}}
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    {{session('status')}} {{-- <- Display the session value --}}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
+
+
 <script src="{{asset('js/app.js')}}"></script> {{-- <- bootstrap and jquery --}}
+<script>
+    //close the alert after 3 seconds.
+    $(document).ready(function(){
+        setTimeout(function() {
+            $(".alert").alert('close');
+        }, 3000);
+    });
+</script>
+
 
 </body>
 </html>
